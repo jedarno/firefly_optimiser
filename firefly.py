106442708bfa4912816feeb601ba35068bf29850
Jed Arno <jedarno99@gmail.com>
2022-12-05 21:32:43 +0000

    Swarm Initialisation

--- firefly.py
+++ firefly.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+def convert_to_bounds(bounds, positions):
+  values = np.zeros_like(positions)
+  
+  for i, pos in enumerate(positions):
+    for j in range(len(bounds)):
+      values[i][j] = bounds[j][0] + ((bounds[j][1] - bounds[j][0]) * pos[j])
+  
+  return values
+
 class Firefly:
 
   def __init__(self,
@@ -16,8 +25,19 @@ class Firefly:
     self.gamma = light_absorption
     self.alpha = randomisation_param
     self.position = None
+    self.attractiveness = None
     self.g_fitness = None
     self.g_best = None
     self.cur_fitness = None
    
+  def initialise_swarm(self):
+    lower_bounds = self.bounds[:,0]
+    upper_bounds = self.bounds[:,1]
+    self.position = np.random.uniform(lower_bounds, upper_bounds, [self.population, len(lower_bounds)])
+    params = convert_to_bounds(self.limits, self.position)
+    self.attractiveness = np.array(list(map(self.function, params)))
+    self.g_fitness = self.attractiveness.max
+    self.g_best = self.position[np.argmin(self.attractiveness)]
+
+
     
--- test_firefly.py
+++ test_firefly.py
@@ -2,6 +2,14 @@ import numpy as np
 
 from firefly import Firefly
 
+def sum_fun(param):
+  sum  = 0
+
+  for num in param:
+    sum += num
+
+  return sum
+
 def rosenbrock(param):
   f = 0
 
@@ -26,4 +34,15 @@ def test_init():
 
   return True
 
+def test_initialise_swarm():
+  swarm = Firefly(function=sum_fun, population = 1, bounds = np.array([[0,10], [0,10]]), light_absorption = 0.5, randomisation_param = 0.5)
+  swarm.initialise_swarm()
+
+  for i, pos in enumerate(swarm.position):
+
+    if swarm.attractiveness[i] != (pos[0] + pos[1]) * 10:
+      return "Attractiveness calculated incorectly, should use fitness function. recieved {}, expected {}".format((pos[0] + pos[1])*10*10*10*10*10*10*10*10, swarm.attractiveness[i])
+
+  print("pos: ", swarm.position)
+  return True
 
